eac90140104944a46ac6a234acd30687bd03fb3e
neoe <d@6610199.xyz>
2025-01-16 22:12:54 +0900

    worked

--- square-queen-b461/src/index.js
+++ square-queen-b461/src/index.js
@@ -7,16 +7,56 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
-
+const QRCode = require ( "qrcode-svg" ) ;
 export default {
-  async fetch(request, env) {
-    const url = new URL(request.url);
-    if (url.pathname.startsWith("/api/")) {
-      // TODO: Add your custom /api/* logic here.
-      return new Response("Ok");
-    }
-    // Passes the incoming request through to the assets binding.
-    // No asset matched this request, so this will evaluate `not_found_handling` behavior.
-    return env.ASSETS.fetch(request);
-  },
-};
+	async fetch ( request , env ) {
+		const url = new URL ( request . url )
+		// console . log ( 'path=' , url . pathname )
+		if ( url . pathname == "/app.json" ) {
+			return app_json ( request , env )
+		} else if ( url . pathname == "/qr.png" ) {
+			return genQr ( request , env , url . search )
+		}
+		// Passes the incoming request through to the assets binding.
+		// No asset matched this request, so this will evaluate `not_found_handling` behavior.
+		return env . ASSETS . fetch ( request )
+	} ,
+}
+async function genQr ( request , env , sr0 ) {
+	let sr = new URLSearchParams ( sr0 )
+	var k = sr . get ( 'k' )
+	console . log ( 'k=' + k )
+	if ( k == '' ) { k = "empty" }
+	const qr = new QRCode ( { content : k , width : 128 , height : 128 } )
+	return new Response ( qr . svg ( ) , { headers : { "Content-Type" : "image/svg+xml" } } )
+}
+async function app_json ( request , env ) {
+	let fd = await request . formData ( )
+	let act = fd . get ( 'act' )
+	switch ( act ) {
+		case 'row' :
+		return Response . json ( { html : await genOneRow ( fd . get ( 'comm' ) ,
+					fd . get ( 'cont' ) , fd . get ( 'lastCls' ) ) } )
+		default : //ver
+		return Response . json ( { ver : 1 } )
+	}
+}
+async function genOneRow ( comm , cont , lastCls ) {
+	var m = { 'comm' : comm , 'cont' : cont }
+	var qrImg = `<img src="qr.png?k=${encodeURIComponent(cont)}" alt="${cont}" />`
+	if ( lastCls == '0' ) {
+		m . qr0 = qrImg
+		m . qr1 = ''
+		m . cls = 'alignL'
+	} else {
+		m . qr0 = ''
+		m . qr1 = qrImg
+		m . cls = 'alignR'
+	}
+	let h = `<div class = '${m.cls}' >
+	<a href = # onclick = 'removeMeRow(this)' > X </a >
+	${ m.qr0 } <i class = comm > ${ m.comm } </i > ${ m.cont } ${ m.qr1 }
+</div > `
+	// console . log ( 'h=' , h )
+	return h
+}
